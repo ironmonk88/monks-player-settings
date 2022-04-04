bef4c81fc5cb0d7a48f8d058be85ac7d7fba5b31
ironmonk88 <75920956+ironmonk88@users.noreply.github.com>
2022-04-04 10:23:30 -0700

    1.0.0 changes

--- monks-player-settings.js
+++ monks-player-settings.js
@@ -1,29 +1,171 @@
 ﻿import { registerSettings } from "./settings.js";
-import { WithMonksSettings } from "./apps/app-settings.js"
+import { MonksSettingsConfig } from "./apps/settings-config.js"
+
+export let debugEnabled = 0;
 
 export let debug = (...args) => {
-    if (debugEnabled > 1) console.log("DEBUG: monks-player-setting | ", ...args);
+    if (debugEnabled > 1) console.log("DEBUG: monks-player-settings | ", ...args);
 };
-export let log = (...args) => console.log("monks-player-setting | ", ...args);
+export let log = (...args) => console.log("monks-player-settings | ", ...args);
 export let warn = (...args) => {
-    if (debugEnabled > 0) console.warn("monks-player-setting | ", ...args);
+    if (debugEnabled > 0) console.warn("monks-player-settings | ", ...args);
 };
-export let error = (...args) => console.error("monks-player-setting | ", ...args);
+export let error = (...args) => console.error("monks-player-settings | ", ...args);
 export let i18n = key => {
     return game.i18n.localize(key);
 };
 export let setting = key => {
-    return game.settings.get("monks-player-setting", key);
+    return game.settings.get("monks-player-settings", key);
 };
 
 export class MonksPlayerSettings {
     static init() {
 
-        MonksPlayerSettings.SOCKET = "module.monks-player-setting";
+        MonksPlayerSettings.SOCKET = "module.monks-player-settings";
 
         registerSettings();
 
-        CONFIG.ui.chat = WithMonksSettings(CONFIG.ui.chat);
+        Object.defineProperty(ClientSettings.prototype, "sheet", {
+            get: function () {
+                if (!this._sheet) this._sheet = (game.user.isGM ? new MonksSettingsConfig() : new SettingsConfig());
+                return this._sheet;
+            }
+        })
+    }
+
+    static async ready() {
+        if (game.user.data.flags == undefined || game.user.data.flags['monks-player-settings'] == undefined)
+            MonksPlayerSettings.saveSettings(); //save what I've got
+
+        MonksPlayerSettings.checkSettings();
+    }
+
+    static async saveSettings() {
+        let clientSettings = duplicate(game.settings.storage.get("client"));
+        await game.user.setFlag('monks-player-settings', 'client-settings', clientSettings);
+    }
+
+    static cleanSetting(settings) {
+        delete settings.core;
+        delete settings[game.system.id];
+        delete settings["monks-player-settings"];
+
+        for (let [module, s] of Object.entries(settings)) {
+            if (typeof s === "object") {
+                for (let name of Object.keys(s)) {
+                    let key = `${module}.${name}`;
+                    let config = game.settings.settings.get(key);
+                    if (!config || !config.config) {
+                        try {
+                            delete s[name];
+                        } catch (err) {
+                            log(err);
+                        }
+                    }
+                }
+                if (Object.keys(settings[module]).length === 0)
+                    delete settings[module];
+            } else
+                delete settings[module];
+        }
+
+        return settings;
+    }
+
+    static async checkSettings() {
+        let refresh = false;
+
+        //if there are differences in the stored settings request to sync
+        let client = this.cleanSetting(expandObject(duplicate(game.settings.storage.get("client")) || {}));
+        let stored = game.user.getFlag('monks-player-settings', 'client-settings');
+        if (stored !== undefined) {
+            stored = this.cleanSetting(duplicate(stored || {}));
+            let diff = diffObject(client, stored);
+
+            if (!isObjectEmpty(diff)) {
+                await Dialog.confirm({
+                    title: `Data Sync`,
+                    content: `<h4>Difference in client settings detected</h4><p>Would you like to sync your current account with the saved settings?</p>`,
+                    yes: () => {
+                        for (let [namespace, values] of Object.entries(diff)) {
+                            for (let [k, v] of Object.entries(values)) {
+                                let key = `${namespace}.${k}`;
+                                window.localStorage.setItem(key, v);
+
+                                console.log(`Setting Sync: ${key}, "${client[namespace][k]}" -> "${v}"`);
+
+                                //do any of the differences call a function?  Then refresh the browser
+                                const setting = game.settings.get(key);
+                                if (setting.onChange instanceof Function) refresh = true;
+                            }
+                        }
+                    }
+                });
+            }
+        }
+
+        //check to see if there are GM changes
+        //and if the webpage needs to be refreshed
+        if (await MonksPlayerSettings.refreshSettings() || refresh)
+            MonksPlayerSettings.checkRefresh();
+    }
+
+    static async refreshSettings() {
+        let refresh = false;
+
+        //if there are GM changes then prompt to update with those changes
+        if (game.user.getFlag('monks-player-settings', 'gm-settings') != undefined) {
+            let gmsettings = game.user.getFlag('monks-player-settings', 'gm-settings');
+            for (let [namespace, values] of Object.entries(gmsettings)) {
+                for (let [k, v] of Object.entries(values)) {
+                    let key = `${namespace}.${k}`;
+                    window.localStorage.setItem(key, v);
+
+                    console.log(`GM Update Setting: ${key}, "${v}"`);
+
+                    //if the webpage needs to be refreshed then return true
+                    const setting = game.settings.settings.get(key);
+                    if (setting.onChange instanceof Function) refresh = true;
+                }
+            }
+
+            //clear the gm setting changes
+            game.user.unsetFlag('monks-player-settings', 'gm-settings');
+
+            //save a new copy of the client settings
+            let clientSettings = this.cleanSetting(expandObject(duplicate(game.settings.storage.get("client"))));
+            await game.user.setFlag('monks-player-settings', 'client-settings', clientSettings);
+
+            ui.notifications.info("GM has made changes to your client settings");
+        }
+
+        return refresh;
+    }
+
+    static checkRefresh() {
+        Dialog.confirm({
+            title: `Browser refresh`,
+            content: `<h4>Some of the settings changed required Foundry to be restarted</h4><p>Reload the browser?</p>`,
+            yes: () => {
+                location.reload();
+            }
+        });
     }
 }
 
+Hooks.once('init', async function () {
+    MonksPlayerSettings.init();
+});
+
+Hooks.once('ready', async function () {
+    MonksPlayerSettings.ready();
+});
+
+Hooks.on('updateUser', async function (user, data, options) {
+    console.log('updating user', user, data, options);
+    //If the GM has changed settings and the player is currently active, then refresh settings
+    if (data.flags && data.flags["monks-player-settings"] && data.flags["monks-player-settings"]["gm-settings"]) {
+        if (await MonksPlayerSettings.refreshSettings())
+            MonksPlayerSettings.checkRefresh();
+    }
+});
--- settings.js
+++ settings.js
@@ -2,7 +2,13 @@ import { MonksPlayerSettings, i18n } from "./monks-player-settings.js";
 
 export const registerSettings = function () {
     // Register any custom module settings here
-    let modulename = "monks-player-settingsg";
+    let modulename = "monks-player-settings";
 	
-	
+	game.settings.register(modulename, "sync-settings", {
+		name: i18n("MonksPlayerSettings.sync-settings.name"),
+		hint: i18n("MonksPlayerSettings.sync-settings.hint"),
+		scope: "client",
+		default: true,
+		type: Boolean
+	});
 };
